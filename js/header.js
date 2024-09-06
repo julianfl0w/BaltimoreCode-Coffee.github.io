322ea9d8f46c479d63bd08bd605de1b4c6c1d5ee
Mark Reed <markdreed512@gmail.com>
2024-09-06 16:12:42 -0400

    mobile menu animation

--- js/header.js
+++ js/header.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", function () {
         const mainNav = document.querySelector('.main-nav')
         if(!mainNav.classList.contains('mobile-open')){
             mainNav.classList.add('mobile-open')
+            mainNav.classList.remove('mobile-closed')
         }else{
+            mainNav.classList.add('mobile-closed')
             mainNav.classList.remove('mobile-open')
         }
     }
